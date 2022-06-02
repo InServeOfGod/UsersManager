e227ad2ce087812f9471d47f6ef16d22994b209b
black <black@DESKTOP-ML0IM13>
2022-06-02 14:58:23 +0300

    Proje dosyası ekle.

---
+++ UsersManager/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UsersManager
+{
+    class User
+    {
+        public string username { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public DateTime registered { get; set; }
+
+        public User(string username, string email, string password)
+        {
+            this.username = username;
+            this.email = email;
+            this.password = password;
+            this.registered = DateTime.Now;
+        }
+
+        public string vars()
+        {
+            return $"username : { this.username } \nemail : { this.email } \npassword :  { this.password } \nregistered : { this.registered }";
+        }
+    }
+}
